9a2dca4943c93851bc43fadd6f2a1ea02bc7a7a3
Tiago <tiagokatcipis@gmail.com>
2014-11-19 22:30:44 -0200

    removing vundle.

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "Installing Vundle"
 rm -rf $HOME/.vim/bundle 
 git clone https://github.com/gmarik/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim 
 echo "Installing Plugins"
-vim +PluginInstall +qall
+#vim +PluginInstall +qall
 echo "Workaround for the vim-scala plugin" #https://github.com/derekwyatt/vim-scala/issues/75
 mkdir -p ~/.vim/ftdetect 
 cp $HOME/.vim/bundle/vim-scala/ftdetect/scala.vim $HOME/.vim/ftdetect
