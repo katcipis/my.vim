36159abb6593c9dcfec7ae80e3c21b9349088d16
Tiago Katcipis <tiagokatcipis@gmail.com>
2023-06-22 14:21:38 +0200

    chore: add autofmt for JSON

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -70,6 +70,7 @@ lsp.format_on_save({
         ['lua_ls'] = { 'lua' },
         ['rust_analyzer'] = { 'rust' },
         ['terraformls'] = { 'terraform' },
+        ['jsonls'] = { 'json' },
     }
 })
 
