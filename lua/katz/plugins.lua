3bc627b55fe310bf529feb93e63b545522b84c64
Tiago Katcipis <tiagokatcipis@gmail.com>
2024-08-26 13:37:51 +0200

    chore: set specific branch for lsp zero

--- lua/katz/plugins.lua
+++ lua/katz/plugins.lua
@@ -42,6 +42,7 @@ return require('packer').startup(function(use)
     }
     use {
         'VonHeikemen/lsp-zero.nvim',
+        branch = "v3.x",
         requires = {
             -- LSP Support
             { 'neovim/nvim-lspconfig' },
